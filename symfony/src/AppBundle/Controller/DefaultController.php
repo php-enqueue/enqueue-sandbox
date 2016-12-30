db93cfdf3d3693f45fb1f51c4a254d73660aaa54
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2016-12-30 14:53:17 +0200

    upd

--- symfony/src/AppBundle/Controller/DefaultController.php
+++ symfony/src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Controller;
 
+use Enqueue\Client\Message;
+use Enqueue\Client\MessageProducerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,6 +15,17 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        /** @var MessageProducerInterface $producer */
+        $producer = $this->get('enqueue.message_producer');
+
+        $producer->send('foo_topic', 'Hello world');
+
+        $producer->send('bar_topic', ['bar' => 'val']);
+
+        $message = new Message();
+        $message->setBody('baz');
+        $producer->send('baz_topic', $message);
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
