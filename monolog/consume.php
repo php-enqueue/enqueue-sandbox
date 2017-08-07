576b605ba573362d05e1c875595f462af5734f5a
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2017-08-07 15:22:16 +0300

    upd

--- monolog/consume.php
+++ monolog/consume.php
@@ -1,12 +1,13 @@
 <?php
 
 use Enqueue\Consumption\QueueConsumer;
+use Enqueue\Fs\FsConnectionFactory;
 use Interop\Queue\PsrMessage;
 use Interop\Queue\PsrProcessor;
 
 require_once __DIR__.'/vendor/autoload.php';
 
-$context = (new \Enqueue\Fs\FsConnectionFactory('file://'.__DIR__.'/queue'))->createContext();
+$context = (new FsConnectionFactory('file://'.__DIR__.'/queue'))->createContext();
 
 $consumer = new QueueConsumer($context);
 $consumer->bind('log', function(PsrMessage $message) {
--- monolog/log.php
+++ monolog/log.php
@@ -1,15 +1,16 @@
 <?php
 
-use Monolog\Handler\QueueInteropHandler;
+use Enqueue\Fs\FsConnectionFactory;
+use Enqueue\Monolog\QueueHandler;
 use Monolog\Logger;
 
 require_once __DIR__.'/vendor/autoload.php';
 
-$context = (new \Enqueue\Fs\FsConnectionFactory('file://'.__DIR__.'/queue'))->createContext();
+$context = (new FsConnectionFactory('file://'.__DIR__.'/queue'))->createContext();
 
 // create a log channel
 $log = new Logger('name');
-$log->pushHandler(new QueueInteropHandler($context));
+$log->pushHandler(new QueueHandler($context));
 
 // add records to the log
 $log->warning('Foo');
